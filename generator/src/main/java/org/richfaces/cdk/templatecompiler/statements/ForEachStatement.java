3ed605d28acaf16071e8498c30b8c6d81abe2b59
Lukas Fryc <lfryc@redhat.com>
2012-05-30 13:13:22 +0200

    RF-10037: CDK: <c:forEach > should generate while(){} loop for Iterator

--- generator/src/main/java/org/richfaces/cdk/templatecompiler/statements/ForEachStatement.java
+++ generator/src/main/java/org/richfaces/cdk/templatecompiler/statements/ForEachStatement.java
@@ -21,6 +21,8 @@
  */
 package org.richfaces.cdk.templatecompiler.statements;
 
+import java.util.Iterator;
+
 import org.richfaces.cdk.Logger;
 import org.richfaces.cdk.generate.freemarker.FreeMarkerRenderer;
 import org.richfaces.cdk.templatecompiler.ELParser;
@@ -28,7 +30,9 @@ import org.richfaces.cdk.templatecompiler.TemplateModel;
 import org.richfaces.cdk.templatecompiler.builder.model.JavaField;
 import org.richfaces.cdk.templatecompiler.builder.model.JavaImport;
 import org.richfaces.cdk.templatecompiler.el.ParsingException;
+import org.richfaces.cdk.templatecompiler.el.types.ComplexType;
 import org.richfaces.cdk.templatecompiler.el.types.ELType;
+import org.richfaces.cdk.templatecompiler.el.types.TypesFactory;
 
 import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
@@ -38,16 +42,22 @@ import com.google.inject.Inject;
  */
 public class ForEachStatement extends FreeMarkerTemplateStatementBase {
     private TypedTemplateStatement itemsStatement;
+    private ELType itemsType;
     private String var;
     private ELType varType;
     private final ELParser parser;
     private final Logger log;
+    private final TypesFactory typesFactory;
+
+    private boolean iterator = false;
+    private boolean iterable = false;
 
     @Inject
-    public ForEachStatement(@TemplateModel FreeMarkerRenderer renderer, ELParser parser, Logger log) {
+    public ForEachStatement(@TemplateModel FreeMarkerRenderer renderer, ELParser parser, Logger log, TypesFactory typesFactory) {
         super(renderer, "for-each");
         this.parser = parser;
         this.log = log;
+        this.typesFactory = typesFactory;
     }
 
     /**
@@ -80,11 +90,32 @@ public class ForEachStatement extends FreeMarkerTemplateStatementBase {
      */
     public void setItemsExpression(String itemsExpression, String var) {
         try {
-            this.itemsStatement = parser.parse(itemsExpression, this, Iterable.class.getName());
+            this.itemsStatement = parser.parse(itemsExpression, this, Object.class.getName());
+            this.itemsType = this.itemsStatement.getType();
             this.itemsStatement.setParent(this);
             this.varType = this.itemsStatement.getType().getContainerType();
             this.var = var;
             setVariable(var, this.varType);
+
+            if (this.itemsType.isArray()) {
+                iterable = true;
+            } else {
+                if (this.itemsType instanceof ComplexType) {
+                    this.itemsType = ((ComplexType) this.itemsType).getRawType();
+                }
+
+                if (typesFactory.getType(Iterable.class).isAssignableFrom(this.itemsType)) {
+                    iterable = true;
+                } else if (typesFactory.getType(Iterator.class).isAssignableFrom(this.itemsType)) {
+                    iterator = true;
+                }
+            }
+
+            if (!iterable && !iterator) {
+                log.info("The <foreach> expression " + itemsExpression
+                        + " is not array, Iterable nor Iterator. It will be treated as single object.");
+            }
+
         } catch (ParsingException e) {
             log.error("Error parsing expression for iteration in <foreach> statement", e);
         }
@@ -103,5 +134,13 @@ public class ForEachStatement extends FreeMarkerTemplateStatementBase {
     @Override
     public Iterable<JavaField> getRequiredFields() {
         return Iterables.concat(super.getRequiredFields(), itemsStatement.getRequiredFields());
+    }
+
+    public boolean isIterable() {
+        return iterable;
+    }
+
+    public boolean isIterator() {
+        return iterator;
     }
 }
--- generator/src/main/resources/META-INF/templates/java/for-each.ftl
+++ generator/src/main/resources/META-INF/templates/java/for-each.ftl
@@ -1,5 +1,19 @@
-for (${varType} ${var}: ${itemsExpression}) {
+<#if iterable>
+	for (${varType} ${var} : ${itemsExpression}) {
+		<#list statements as subStatement>
+		${subStatement.code}
+		</#list>
+	}
+<#elseif iterator>
+	while (${itemsExpression}.hasNext()) {
+	    ${varType} ${var} = ${itemsExpression}.next();
+	    <#list statements as subStatement>
+		${subStatement.code}
+		</#list>
+	}
+<#else>
+	${varType} ${var} = ${itemsExpression};
 	<#list statements as subStatement>
 	${subStatement.code}
 	</#list>
-}
+</#if>
