f784d93a074d5c17b27ac19f76b85c4ecd51cf14
Lukas Fryc <lfryc@redhat.com>
2012-08-08 16:41:37 +0200

    changing versions back to development: 4.3.0-SNAPSHOT

--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedComponent.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedComponent.java
@@ -43,7 +43,7 @@ import javax.faces.el.MethodBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.0.20120802-M1"})
+@Generated({"RichFaces CDK", "4.3.0-SNAPSHOT"})
 public class GeneratedComponent extends UIOutput
     implements ClientBehaviorHolder
     {
--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
@@ -32,7 +32,7 @@ import javax.faces.el.ValueBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.0.20120802-M1"})
+@Generated({"RichFaces CDK", "4.3.0-SNAPSHOT"})
 public class GeneratedValidator extends Object implements Validator, PartialStateHolder {
 
     @Override
