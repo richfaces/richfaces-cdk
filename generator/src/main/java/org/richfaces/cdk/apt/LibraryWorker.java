571d2d76f899f6731e4d4423b9a561cae47936a1
Lukas Fryc <lfryc@redhat.com>
2012-08-24 13:22:02 +0200

    create LibraryWorker interface

--- generator/src/main/java/org/richfaces/cdk/apt/LibraryWorker.java
+++ generator/src/main/java/org/richfaces/cdk/apt/LibraryWorker.java
@@ -1,81 +1,22 @@
 package org.richfaces.cdk.apt;
 
-import java.util.Set;
-
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 
 import org.richfaces.cdk.CdkException;
-import org.richfaces.cdk.CdkWriter;
-import org.richfaces.cdk.Logger;
-import org.richfaces.cdk.ModelBuilder;
-import org.richfaces.cdk.ModelValidator;
-import org.richfaces.cdk.model.ComponentLibrary;
 
-import com.google.inject.Inject;
+public interface LibraryWorker {
 
-public class LibraryWorker {
+    public abstract void beforeJavaSourceProcessing();
 
-    @Inject
-    private Logger log;
-    
-    @Inject
-    JavaSourceProcessor javaSourceProcessor;
-    
-    @Inject
-    private Set<CdkWriter> generators;
+    public abstract void processJavaSource(ProcessingEnvironment processingEnv, RoundEnvironment roundEnv);
 
-    @Inject
-    private ComponentLibrary library;
+    public abstract void afterJavaSourceProcessing();
 
-    @Inject
-    private Set<ModelBuilder> builders;
-    @Inject
-    private ModelValidator validator;
+    public abstract void generate() throws CdkException;
 
-    protected void beforeJavaSourceProcessing() {
-    }
-    
-    protected void processJavaSource(ProcessingEnvironment processingEnv, RoundEnvironment roundEnv) {
-        javaSourceProcessor.process(processingEnv, roundEnv);
-    }
-    
-    protected void afterJavaSourceProcessing() {
-        processNonJavaSources();
-        verify();
-        if (0 == log.getErrorCount()) {
-            generate();
-        }
-    }
-    
-    protected void generate() throws CdkException {
-        if (0 == log.getErrorCount()) {
-            // processing over, generate files.
-            for (CdkWriter generator : generators) {
-                generator.render(library);
-            }
-        }
-    }
+    public abstract void processNonJavaSources() throws CdkException;
 
-    protected void processNonJavaSources() throws CdkException {
-        for (ModelBuilder builder : builders) {
-            log.debug("Run builder " + builder.getClass().getName());
-            try {
-                builder.build();
-            } catch (CdkException e) {
-                // TODO: sendError(e);
-                e.printStackTrace();
-            }
-        }
-    }
+    public abstract void verify() throws CdkException;
 
-    protected void verify() throws CdkException {
-        try {
-            log.debug("Validate model");
-            validator.verify(library);
-        } catch (CdkException e) {
-            // TODO: sendError(e);
-            e.printStackTrace();
-        }
-    }
 }
---
+++ generator/src/main/java/org/richfaces/cdk/apt/LibraryWorkerImpl.java
@@ -0,0 +1,105 @@
+package org.richfaces.cdk.apt;
+
+import java.util.Set;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+
+import org.richfaces.cdk.CdkException;
+import org.richfaces.cdk.CdkWriter;
+import org.richfaces.cdk.Logger;
+import org.richfaces.cdk.ModelBuilder;
+import org.richfaces.cdk.ModelValidator;
+import org.richfaces.cdk.model.ComponentLibrary;
+
+import com.google.inject.Inject;
+
+public class LibraryWorkerImpl implements LibraryWorker {
+
+    @Inject
+    private Logger log;
+    
+    @Inject
+    JavaSourceProcessor javaSourceProcessor;
+    
+    @Inject
+    private Set<CdkWriter> generators;
+
+    @Inject
+    private ComponentLibrary library;
+
+    @Inject
+    private Set<ModelBuilder> builders;
+    @Inject
+    private ModelValidator validator;
+
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#beforeJavaSourceProcessing()
+     */
+    @Override
+    public void beforeJavaSourceProcessing() {
+    }
+    
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#processJavaSource(javax.annotation.processing.ProcessingEnvironment, javax.annotation.processing.RoundEnvironment)
+     */
+    @Override
+    public void processJavaSource(ProcessingEnvironment processingEnv, RoundEnvironment roundEnv) {
+        javaSourceProcessor.process(processingEnv, roundEnv);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#afterJavaSourceProcessing()
+     */
+    @Override
+    public void afterJavaSourceProcessing() {
+        processNonJavaSources();
+        verify();
+        if (0 == log.getErrorCount()) {
+            generate();
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#generate()
+     */
+    @Override
+    public void generate() throws CdkException {
+        if (0 == log.getErrorCount()) {
+            // processing over, generate files.
+            for (CdkWriter generator : generators) {
+                generator.render(library);
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#processNonJavaSources()
+     */
+    @Override
+    public void processNonJavaSources() throws CdkException {
+        for (ModelBuilder builder : builders) {
+            log.debug("Run builder " + builder.getClass().getName());
+            try {
+                builder.build();
+            } catch (CdkException e) {
+                // TODO: sendError(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.richfaces.cdk.apt.LibraryWorker#verify()
+     */
+    @Override
+    public void verify() throws CdkException {
+        try {
+            log.debug("Validate model");
+            validator.verify(library);
+        } catch (CdkException e) {
+            // TODO: sendError(e);
+            e.printStackTrace();
+        }
+    }
+}
