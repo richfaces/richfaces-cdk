2742762f73019b9712a54110200851a8f103de30
Lukas Fryc <lfryc@redhat.com>
2013-01-09 13:42:48 +0100

    changing version to 4.3.0.CR1

--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
@@ -32,7 +32,7 @@ import javax.faces.el.ValueBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.0-SNAPSHOT"})
+@Generated({"RichFaces CDK", "4.3.0.CR1"})
 public class GeneratedValidator extends Object implements Validator, PartialStateHolder {
 
     @Override
