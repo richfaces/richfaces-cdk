e815aa990478afa66359b4510d76a32fed90fab5
Lukas Fryc <lfryc@redhat.com>
2013-01-24 08:06:21 +0100

    changing versions back to development: 4.3.0-SNAPSHOT

--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedBehavior.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedBehavior.java
@@ -31,7 +31,7 @@ import javax.faces.el.ValueBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.0.CR2"})
+@Generated({"RichFaces CDK", "4.3.0-SNAPSHOT"})
 public class GeneratedBehavior extends Object implements Behavior, PartialStateHolder {
 
     @Override
