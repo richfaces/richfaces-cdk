16521f334974f0a1bbdfd1deb0407cfdfae40654
Brian Leathem <bleathem@gmail.com>
2013-01-30 00:39:56 -0800

    changing version to 4.3.0.Final

--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedComponent.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedComponent.java
@@ -43,7 +43,7 @@ import javax.faces.el.MethodBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.0-SNAPSHOT"})
+@Generated({"RichFaces CDK", "4.3.0.Final"})
 public class GeneratedComponent extends UIOutput
     implements ClientBehaviorHolder
     {
