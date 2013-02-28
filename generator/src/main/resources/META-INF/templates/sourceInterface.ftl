cd8df06f8d8bb0ad7b351150a29fe4b1b70858c0
Lukas Fryc <lfryc@redhat.com>
2013-02-28 11:08:29 +0100

    changing versions back to development: 4.3.1-SNAPSHOT

--- generator/src/main/resources/META-INF/templates/sourceInterface.ftl
+++ generator/src/main/resources/META-INF/templates/sourceInterface.ftl
@@ -8,7 +8,7 @@ import ${listenerInterface};
 /**
  * ${description?if_exists}
  **/
-@Generated({"RichFaces CDK", "4.3.1.CR1"})
+@Generated({"RichFaces CDK", "4.3.1-SNAPSHOT"})
 public interface ${sourceInterface.simpleName} {
 
     /**
--- generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
+++ generator/src/test/resources/org/richfaces/cdk/generate/java/GeneratedValidator.java
@@ -32,7 +32,7 @@ import javax.faces.el.ValueBinding;
 /**
  * 
  **/
-@Generated({"RichFaces CDK", "4.3.1.CR1"})
+@Generated({"RichFaces CDK", "4.3.1-SNAPSHOT"})
 public class GeneratedValidator extends Object implements Validator, PartialStateHolder {
 
     @Override
