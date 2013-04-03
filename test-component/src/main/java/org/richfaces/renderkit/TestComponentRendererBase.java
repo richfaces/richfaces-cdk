548d99cd6571c7f1fda3fd53508123f4d1baca33
Lukas Fryc <lfryc@redhat.com>
2013-04-03 18:42:50 +0200

    RF-12881: separating the CDK build from the Framework

--- test-component/src/main/java/org/richfaces/renderkit/TestComponentRendererBase.java
+++ test-component/src/main/java/org/richfaces/renderkit/TestComponentRendererBase.java
@@ -2,10 +2,11 @@ package org.richfaces.renderkit;
 
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
+import javax.faces.render.Renderer;
 
 @ResourceDependencies({
         @ResourceDependency(library = "org.richfaces", name = "testComponent.js"),
         @ResourceDependency(library = "org.richfaces", name = "testComponent.css")})
-public abstract class TestComponentRendererBase extends RendererBase {
+public abstract class TestComponentRendererBase extends Renderer {
     public static final String RENDERER_TYPE = "org.richfaces.TestComponentRenderer";
 }
